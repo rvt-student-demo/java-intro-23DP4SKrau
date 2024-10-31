c405f30b6a56231ce771f058cef2691814392706
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-10-31 12:05:13 +0000

    Leap year

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,26 +6,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+int number = Integer.valueOf(reader.nextLine());
 
-        System.out.println("Give points [0-100]:");
-        double score = Double.valueOf(scanner.nextLine());
-        if (score > 100) {
-            System.out.println("Grade: incredible!");  
-        } else if (score < 100 && score > 90) {
-            System.out.println("Grade: 5");
-        }else if (score < 89 && score > 80) {
-            System.out.println("Grade: 4");
-        }else if (score < 79 && score > 70) {
-            System.out.println("Grade: 3");
-        }else if (score < 69 && score > 60) {
-            System.out.println("Grade: 2");
-        }else if (score < 59 && score > 50) {
-            System.out.println("Grade: 1");
-        }else if (score < 49 && score > 0) {
-            System.out.println("Grade: failed");
-        } else {
-            System.out.println("Grade: Impossible!");  
-        }
-    }
+  if (number % 4 != 0) {
+    System.out.println("The year is not a leap year.");
+} if (number % 5 == 0) {
+    System.out.println("The number is divisible by five!");
+} if (number % 6 != 0) {
+    System.out.println("The number is not divisible by six!");
+}
+   }
 }
