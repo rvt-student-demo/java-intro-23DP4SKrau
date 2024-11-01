7d5caada2347ec36eb07fd6d03342c8b7cd10743
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-11-01 10:34:34 +0000

    Exercise 2 — Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,17 +4,18 @@ import java.util.*;
 
 public class App 
 {
-    
     public static void main ( String[] args )
-    {
-        int[] val = {0, 1, 2, 3}; 
+  {
+    int[] val = {13, -4, 82, 17}; 
+    int[] twice = new int[4];
 
-        int sum = 0;
-        for(int i =0; i < val.length; i++) {
-           sum += val[i];
-       }
+    System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
  
-         System.out.println( "Sum of all numbers = " + sum );
- 
+    twice[0] = val[0] * 2;
+    twice[0] = val[1] * 2;
+    twice[0] = val[2] * 2;
+    twice[0] = val[3] * 2;
+
+    System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
    }
 }
