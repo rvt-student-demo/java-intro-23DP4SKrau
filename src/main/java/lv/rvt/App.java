a1cd44164982d98aa429f8153fef22abaf455466
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-11-01 11:00:50 +0000

    Exercise 3 — Three Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,21 +1,25 @@
 package lv.rvt;
 
-import java.util.*;
+// import java.util.*;
 
 public class App 
 {
-    public static void main ( String[] args )
+  public static void main ( String[] args )
   {
-    int[] val = {13, -4, 82, 17}; 
-    int[] twice = new int[4];
+    int[] valA   = { 13, -22,  82,  17}; 
+    int[] valB   = {-12,  24, -79, -13};
+    int[] sum    = {  0,   0,   0,   0};
+  
 
-    System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+    sum[0] = valA[0] + valB[0];
+    sum[1] = valA[1] + valB[1];
+    sum[2] = valA[2] + valB[2];
+    sum[3] = valA[3] + valB[3];
+
  
-    twice[0] = val[0] * 2;
-    twice[0] = val[1] * 2;
-    twice[0] = val[2] * 2;
-    twice[0] = val[3] * 2;
-
-    System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+    System.out.println( "sum: " 
+        + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
    }
 }
+
+
