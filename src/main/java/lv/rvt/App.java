079e18c9605d0ea4c07fe0b0c56621d994d70ef4
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-11-01 11:07:43 +0000

    Exercise 4 — Same Sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,18 +7,17 @@ public class App
   public static void main ( String[] args )
   {
     int[] valA   = { 13, -22,  82,  17}; 
-    int[] valB   = {-12,  24, -79, -13};
-    int[] sum    = {  0,   0,   0,   0};
-  
-
-    sum[0] = valA[0] + valB[0];
-    sum[1] = valA[1] + valB[1];
-    sum[2] = valA[2] + valB[2];
-    sum[3] = valA[3] + valB[3];
-
+    int[] valB   = {  0,   0,   0,   0};
+    
+    for(int i = 0; i < 4; i++){
+      valB[i] = 25 - valA[i];
+    }
  
-    System.out.println( "sum: " 
-        + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+    System.out.println( "valA: " + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+ 
+    System.out.println( "valB: " + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+
+    System.out.println( "sum:  " + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
    }
 }
 
