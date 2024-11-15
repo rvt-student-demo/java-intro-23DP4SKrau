c3430735e5638fd1cf1f50c18512dc53f4110ed1
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-11-15 12:09:59 +0000

    paymentcard

---
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -0,0 +1,21 @@
+package lv.rvt;
+import java.util.*;
+ 
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        this.balance = openingBalance;   
+    }
+
+    public String toString() {
+        return "The card has a balance of " + this.balance + " euros";
+    }
+}
+public class MainProgram {
+    public static void main(String[] args) {
+        PaymentCard card = new PaymentCard(50);
+        System.out.println(card);
+    }
+}
+
