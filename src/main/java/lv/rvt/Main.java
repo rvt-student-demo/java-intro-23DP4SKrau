bec0696b2a8988af396243222d087e69b32d26dc
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-11-29 11:50:41 +0000

    Main

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -2,14 +2,11 @@ package lv.rvt;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a HealthStation object
         HealthStation childrensHospital = new HealthStation();
 
-        // Create two Person objects with sample data
         Person ethan = new Person("Ethan", 1, 110, 7);
         Person peter = new Person("Peter", 33, 176, 85);
 
-        // Print out their weights using the weigh method of HealthStation
         System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
         System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
     }
