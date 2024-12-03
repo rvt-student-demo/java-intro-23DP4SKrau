873355de35b6ca7a9962385dd2c8ff0c80be5067
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2024-12-03 07:09:14 +0000

    konsultacijas 2.

--- src/main/java/lv/rvt/Main.java
+++ src/main/java/lv/rvt/Main.java
@@ -1,14 +1,29 @@
 package lv.rvt;
+// Programma:
+        // Paluugt lietotaju ievadit massiva garumu
+        // Ar ciklu papildinat doto masivu ar lietotaja ievaditiem datiem
+
+import java.util.Scanner;
+
+// element count: 5
+        // Please input element nr 1: 256
+        // Please input element nr 2: -233
+
+        // Saskaitam masiva elementu summu un elementu videjo vertibu
 
 public class Main {
     public static void main(String[] args) {
-        HealthStation childrensHospital = new HealthStation();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input element count: ");
+        int elementCount = scanner.nextInt();
+        int[] elements = new int[elementCount];
 
-        Person ethan = new Person("Ethan", 1, 110, 7);
-        Person peter = new Person("Peter", 33, 176, 85);
-
-        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+        for (int i = 0; i < elementCount; i++) {
+            System.out.println("Please input element nr." + (i + 1) + ": ");
+            elements[i] = scanner.nextInt();
+          
+        }
+        
     }
-    
-}
+
+  }          
