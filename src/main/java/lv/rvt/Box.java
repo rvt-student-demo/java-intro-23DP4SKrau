8c59257584c3370dfc24df3354babf474626a9e3
Sanija Kraukle <166509320+23DP4SKrau@users.noreply.github.com>
2025-01-17 12:59:58 +0000

    Box uzdevums

--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -1,24 +1,39 @@
+package lv.rvt;
+
 public class Box {
-    private double length, width, height;
+    private double width, height, length;
 
-    public Box(double length, double width, double height) {
-        this.length = length;
-        this.width= width;
+    public Box(double width, double height, double length) {
+        this.width = width;
         this.height = height;
-    }
-    public double area() {
-        return 2 * (length * width + width * height + height * length);
-    }
-    public double volume() {
-        return length * width * height;
+        this.length = length;
     }
 
-    public double getLength() { return length; }
+    public Box(double side) {
+        this(side, side, side);
+    }
+
+    public Box(Box other) {
+        this(other.width, other.height, other.length);
+    }
+
+    public double volume() {
+        return width * height * length;
+    }
+
+    public double area() {
+        return 2 * (width * height + height * length + length * width);
+    }
+
+    public Box biggerBox() {
+        return new Box(width * 1.25, height * 1.25, length * 1.25);
+    }
+
+    public Box smallerBox() {
+        return new Box(width * 0.75, height * 0.75, length * 0.75);
+    }
+
     public double getWidth() { return width; }
     public double getHeight() { return height; }
-
-}
-
-public class BoxTester {
-    public staticvoid 
+    public double getLength() { return length; }
 }
--- src/main/java/lv/rvt/BoxTester.java
+++ src/main/java/lv/rvt/BoxTester.java
@@ -1,28 +1,16 @@
-public class Box {
-    private double length, width, height;
-
-    public Box(double length, double width, double height) {
-        this.length = length;
-        this.width= width;
-        this.height = height;
-    }
-    public double area() {
-        return 2 * (length * width + width * height + height * length);
-    }
-    public double volume() {
-        return length * width * height;
-    }
-
-    public double getLength() { return length; }
-    public double getWidth() { return width; }
-    public double getHeight() { return height; }
-
-}
+package lv.rvt;
 
 public class BoxTester {
     public static void main(String[] args) {
-        Box box = new Box(2.5, 5.0, 6.0);
-        System.out.println("Area: " + box.area() + " Volume: " + box.volume());
-        System.out.println("Length: " + box.getLength() + " Height: " + box.getHeight() + " Width: " + box.getWidth());
+        Box box1 = new Box(2.5, 5.0, 6.0);
+        System.out.println("Area: " + box1.area() + " Volume: " + box1.volume());
+
+        Box box2 = new Box(box1);
+        System.out.println("Box 2 (copy of Box 1) Area: " + box2.area());
+
+        Box bigger = box1.biggerBox();
+        Box smaller = box1.smallerBox();
+        System.out.println("Bigger Box Area: " + bigger.area());
+        System.out.println("Smaller Box Area: " + smaller.area());
     }
 }
